d05f89fb8f5ed2f2fcbf0a7efc3bf84868d5099d
Bit Handshake <adam@monotech.hu>
2022-12-01 17:09:43 +0100

    0.0.1.6

--- source-code/clj/git/side_effects.clj
+++ source-code/clj/git/side_effects.clj
@@ -1,6 +1,7 @@
 
 (ns git.side-effects
     (:require [candy.api  :refer [return]]
+              [git.config :as config]
               [io.api     :as io]
               [string.api :as string]))
 
@@ -8,59 +9,84 @@
 ;; ----------------------------------------------------------------------------
 
 (defn get-gitignore
+  ; @param (map)(opt) options
+  ;  {:filepath (string)(opt)
+  ;    Default: ".gitignore"}
+  ;
   ; @usage
   ; (get-gitignore)
   ;
+  ; @usage
+  ; (get-gitignore {:filepath "my-directory/.gitignore"})
+  ;
   ; @return (string)
-  []
-  (io/read-file ".gitignore"))
+  ([]
+   (get-gitignore {}))
+
+  ([{:keys [filepath] :or {filepath config/DEFAULT-GITIGNORE-FILEPATH}}]
+   (io/read-file filepath)))
 
 (defn ignored?
   ; @param (string) pattern
+  ; @param (map)(opt) options
+  ;  {:filepath (string)(opt)
+  ;    Default: ".gitignore"}
   ;
   ; @usage
   ; (ignored? "my-file.ext")
   ;
+  ; @usage
+  ; (ignored? "my-file.ext" {:filepath "my-directory/.gitignore"})
+  ;
   ; @return (boolean)
-  [pattern]
-  (string/contains-part? (get-gitignore)
-                         (str "\n"pattern"\n")))
+  ([pattern]
+   (ignored? pattern {}))
+
+  ([pattern options]
+   (string/contains-part? (get-gitignore options)
+                          (str "\n"pattern"\n"))))
 
 (defn ignore!
   ; @param (string) pattern
-  ; @param (string)(opt) block-name
-  ;  Default: "git-api"
+  ; @param (map)(opt) options
+  ;  {:block (string)(opt)
+  ;    Default: "git-api"
+  ;   :filepath (string)(opt)
+  ;    Default: ".gitignore"}
   ;
   ; @usage
   ; (ignore! "my-file.ext")
   ;
   ; @usage
-  ; (ignore! "my-file.ext" "My ignored files")
+  ; (ignore! "my-file.ext" {:block "My ignored files"})
+  ;
+  ; @usage
+  ; (ignore! "my-file.ext" {:filepath "my-directory/.gitignore"})
   ;
   ; @example
-  ; (ignore! "my-file.ext" "My ignored files")
+  ; (ignore! "my-file.ext" {:block "My ignored files"})
   ; =>
   ; "\n# My ignored files\nmy-file.ext\n"
   ;
   ; @return (string)
   ; Returns with the updated .gitignore file's content.
   ([pattern]
-   (ignore! pattern "git-api"))
+   (ignore! pattern {}))
 
-  ([pattern block-name]
-   (let [gitignore (get-gitignore)]
-        (letfn [(block-exists?    [block-name] (string/contains-part? gitignore (str "# "block-name)))
-                (write-gitignore! [gitignore]  (println (str "git.api adding pattern to .gitignore: \""pattern"\""))
-                                               (io/write-file! ".gitignore" gitignore {:create? true})
-                                               (return gitignore))]
-               (cond (ignored? pattern)
-                     (return gitignore)
-                     (block-exists? block-name)
-                     (let [gitignore (str (string/to-first-occurence gitignore (str "# "block-name))
+  ([pattern {:keys [block] :or {block "git-api"} :as options}]
+   (let [gitignore (get-gitignore options)]
+        (letfn [(block-exists?    [block]     (string/contains-part? gitignore (str "# "block)))
+                (write-gitignore! [gitignore] (println (str "git.api adding pattern to .gitignore: \""pattern"\""))
+                                              (io/write-file! ".gitignore" gitignore {:create? true})
+                                              (return gitignore))]
+               (cond (ignored?      pattern options)
+                     (return        gitignore)
+                     (block-exists? block)
+                     (let [gitignore (str (string/to-first-occurence gitignore (str "# "block))
                                           (str "\n"pattern)
-                                          (string/after-first-occurence gitignore (str "# "block-name)))]
+                                          (string/after-first-occurence gitignore (str "# "block)))]
                           (write-gitignore! gitignore))
                      :else
                      (let [gitignore (str (string/ends-with! gitignore "\n")
-                                          (str "\n# "block-name"\n"pattern"\n"))]
+                                          (str "\n# "block"\n"pattern"\n"))]
                           (write-gitignore! gitignore)))))))
