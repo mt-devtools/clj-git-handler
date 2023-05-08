50971abddca0934f7c87e1184687e6718ab9abba
Bit Handshake <adam@monotech.hu>
2023-05-08 19:05:20 +0200

    0.0.7.2

--- source-code/clj/git_handler/submodule_updater/updater/env.clj
+++ source-code/clj/git_handler/submodule_updater/updater/env.clj
@@ -121,7 +121,6 @@
   [options submodule-path branch]
   (if-let [commit-message-f (get-config-item options submodule-path :commit-message-f (fn [%] (time/timestamp-string)))]
           (if-let [latest-local-commit-message (get-latest-local-commit-message options submodule-path branch)]
-                  (do (println "a")
-                   (commit-message-f latest-local-commit-message))
+                  (commit-message-f latest-local-commit-message)
                   (core.env/error-catched (str "Cannot read latest local commit message of submodule: " submodule-path " on branch: " branch)))
           (core.env/error-catched (str "Unable to read config item: " submodule-path))))
