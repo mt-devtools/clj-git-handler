6c393a903bcf4b00df63fa86d1595fe38d6a63f4
Bit Handshake <adam@monotech.hu>
2023-12-14 15:56:12 +0100

    0.1.3.1

--- source-code/clj/git_handler/submodule_updater/detector/side_effects.clj
+++ source-code/clj/git_handler/submodule_updater/detector/side_effects.clj
@@ -51,11 +51,11 @@
   ;   Default: ["submodules"]}
   [{:keys [source-paths] :or {source-paths ["submodules"]}}]
   (reset! submodule-updater.detector.state/DETECTED-SUBMODULES nil)
-  (letfn [; Cuts the "/.git" part from end of the '.git' file's path
+  (letfn [; Cuts off the "/.git" part from end of the '.git' file's path
           ; "my-submodule/.git" => "my-submodule"
           (cut-f [git-file] (-> git-file (string/not-ends-with! "/.git")))
 
-          ; Cuts the "/.git" part from the end of all '.git' file's paths in the given vector.
+          ; Cuts off the "/.git" part from the end of all '.git' file's paths in the given vector.
           ; ["my-submodule/.git"] => ["my-submodule"]
           (go-up-f [git-files] (-> git-files (vector/->items cut-f)))
 
